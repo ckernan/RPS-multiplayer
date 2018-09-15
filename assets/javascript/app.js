b5d076aa606f8d9d024586da1d7c7a5a49290179
Git-C <41130164+ckernan@users.noreply.github.com>
2018-09-15 16:11:45 -0500

    two persistant players

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,81 @@
- //$(document).on('ready', function() {
-// var config = {
-//     apiKey: "AIzaSyD57mpiCb94D-OoURYsN90FDCIw4wIrSQQ",
-//     authDomain: "rps-multiplayer-5001d.firebaseapp.com",
-//     databaseURL: "https://rps-multiplayer-5001d.firebaseio.com",
-//     projectId: "rps-multiplayer-5001d",
-//     storageBucket: "",
-//     messagingSenderId: "1011081787788"
-//   };
-//   firebase.initializeApp(config);
-var database = firebase.database();
+$(function () {
+    // var config = {
+    //     apiKey: "AIzaSyD57mpiCb94D-OoURYsN90FDCIw4wIrSQQ",
+    //     authDomain: "rps-multiplayer-5001d.firebaseapp.com",
+    //     databaseURL: "https://rps-multiplayer-5001d.firebaseio.com",
+    //     projectId: "rps-multiplayer-5001d",
+    //     storageBucket: "",
+    //     messagingSenderId: "1011081787788"
+    //   };
+    //   firebase.initializeApp(config);
+    var database = firebase.database();
+    var ref = database.ref("/users");
+    ref.once('value')
+        .then(function (snapshot) {
+            if (snapshot.child("player1").exists()) {
+                var player1 = snapshot.child("player1");
+                $("#player-1-name").html('<h2>' + player1.child("name").val() + '</h2>');
+            };
+            if (snapshot.child("player2").exists()) {
+                var player2 = snapshot.child("player2");
+                $("#player-2-name").html('<h2>' + player2.child("name").val() + '</h2>');
+            };
+            
 
-var user = {
-    name: "",
-    choice: "",
-    wins: 0,
-    losses: 0,
-    ties: 0
-};
+            // $("#player-1-choice").append('<h5>Wins: ' + user.wins + ' Losses: ' + user.losses + ' Ties: ' + user.ties + '</h5>');
+        })
+
+
+    console.log("yo", ref);
+
+    /*var user = {
+        name: "",
+        choice: "",
+        wins: 0,
+        losses: 0,
+        ties: 0
+    };*/
 
     console.log('text')
-  // Signs-in Friendly Chat.
-    $("#button").on('click', function() {
+    // Signs-in Friendly Chat.
+    $("#submit").on('click', function () {
         signIn();
-        $("#name").val("");
+
     });
 
-    
+
     function signIn() {
-        console.log("test")
-        var userName = $("#name").val().trim();
-        console.log(userName);
-        $("#player-1-name").html('<h2>' + userName + '</h2>');
-        $("#player-1-choice").append('<h5>Wins: ' + user.wins + ' Losses: ' + user.losses +   ' Ties: ' + user.ties + '</h5>');
-        database.ref("/users/").child("/name/").set(userName);
-        
+        var ref = database.ref("/users");
+        ref.once('value')
+            .then(function (snapshot) {
+                if (snapshot.child("player1").exists()) {
+                    var userName = $("#name").val().trim();
+                    $("#player-2-name").html('<h2>' + userName + '</h2>');
+                    // $("#player-2-choice").append('<h5>Wins: ' + user.wins + ' Losses: ' + user.losses + ' Ties: ' + user.ties + '</h5>');
+                    database.ref("/users/").child("/player2/").child("/name/").set(userName);
+                } else {
+                    var userName = $("#name").val().trim();
+                    $("#player-1-name").html('<h2>' + userName + '</h2>');
+                    // $("#player-2-choice").append('<h5>Wins: ' + user.wins + ' Losses: ' + user.losses + ' Ties: ' + user.ties + '</h5>');
+                    database.ref("/users/").child("/player1/").child("/name/").set(userName);
+                }
+            });
+
+        // $("#name").val("");
+
+
         // Sign in Firebase using popup auth and Google as the identity provider.
-    // var provider = new firebase.auth.GoogleAuthProvider();
+        // var provider = new firebase.auth.GoogleAuthProvider();
         // firebase.auth().signInWithPopup(provider);
     };
 
     function userChoice() {
-        $("button").on("click", function() {
+        $("button").on("click", function () {
             // In this case, the "this" keyword refers to the button that was clicked
             var choice = $(this).attr("data-choice");
-    });
-};
-//})
+        });
+    };
+})
+
+// database.ref('/users').on('value', function (snapshot) {
+
